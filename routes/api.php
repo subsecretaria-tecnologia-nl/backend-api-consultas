7c6902272d5f6ab776f1d803ccd54880dc13fa56
Juan Carlos Cruz Bautista <CUBJ960215UIA@genl.nl.gob>
2023-06-29 14:58:55 -0600

    create user access api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+//Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/register', [Api\AuthController::class, 'createUser']);
+Route::post('/auth/login', [Api\AuthController::class, 'loginUser']);
